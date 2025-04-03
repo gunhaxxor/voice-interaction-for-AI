7183e07e09a1ae965520f4edae343dbd18f849b5
Gunnar Oledal <gunnar.oledal@gmail.com>
2025-04-03 15:46:50 +0200

    page for testing the speech module

---
+++ pages/speechTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex my-16 mx-auto w-xl" @submit.prevent="addSpeech()">
+    <UInput class="grow" v-model="text" />
+    <UButton type="submit" @click="addSpeech()">Add to speech queue</UButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+function addSpeech() {
+  addSpeechToQueue(text.value);
+}
+
+const { addSpeechToQueue } = initiatateSpeechSynth();
+  
+const text = ref('');
+
+</script>
+
+<style>
+
+</style>
