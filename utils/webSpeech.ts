416cfde3c07896a2fb3a8bb38796f5a1debdb8c0
Gunnar Oledal <gunnar.oledal@gmail.com>
2025-04-03 15:46:30 +0200

    started on speech util module

---
+++ utils/webSpeech.ts
@@ -0,0 +1,77 @@
+import { options } from "marked";
+
+export function isSpeechSynthesisSupported() {
+  if(!window) return false
+  return 'speechSynthesis' in window;
+}
+
+export interface UtteranceOptions {
+  /**
+   * Language for SpeechSynthesis
+   *
+   * @default 'en-US'
+   */
+  lang?: SpeechSynthesisUtterance['lang']
+  /**
+   * Gets and sets the pitch at which the utterance will be spoken at.
+   *
+   * @default 1
+   */
+  pitch?: SpeechSynthesisUtterance['pitch']
+  /**
+   * Gets and sets the speed at which the utterance will be spoken at.
+   *
+   * @default 1
+   */
+  rate?: SpeechSynthesisUtterance['rate']
+  /**
+   * Gets and sets the voice that will be used to speak the utterance.
+   */
+  voice?: SpeechSynthesisVoice
+  /**
+   * Gets and sets the volume that the utterance will be spoken at.
+   *
+   * @default 1
+   */
+  volume?: SpeechSynthesisUtterance['volume']
+}
+type definedUtteranceOptions = Required<Omit<UtteranceOptions, 'voice'>> & Pick<UtteranceOptions, 'voice'>
+
+export function getAvailableVoices() {
+  if(!isSpeechSynthesisSupported()){
+    console.warn('SpeechSynthesis is not supported on this device');
+    return;
+  }
+
+  return window.speechSynthesis.getVoices();
+}
+
+export function initiatateSpeechSynth(defaultUtteranceOptions: UtteranceOptions = {}) {
+  if(!isSpeechSynthesisSupported()){
+    console.warn('SpeechSynthesis is not supported on this device');
+    throw new Error('SpeechSynthesis is not supported on this device. Chech with isSpeechSynthesisSupported() before init');
+  }
+  // const defaultUttOpts = defaultUtteranceOptions as (Required<Omit<UtteranceOptions, 'voice'>> & Pick<UtteranceOptions, 'voice'>);
+
+  const {
+    lang = 'en-US',
+    pitch = 1,
+    rate = 1,
+    voice = null,
+    volume = 1 
+  } = defaultUtteranceOptions;
+  const synth = window.speechSynthesis
+  const addSpeechToQueue = (text: string, options: UtteranceOptions = {}) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = options.lang ?? lang;
+    utterance.pitch = options.pitch ?? pitch;
+    utterance.rate = options.rate ?? rate;
+    utterance.voice = options.voice ?? voice;
+    utterance.volume = options.volume ?? volume;
+    synth.speak(utterance);
+  }
+  
+  return {
+    addSpeechToQueue
+  }
+}
