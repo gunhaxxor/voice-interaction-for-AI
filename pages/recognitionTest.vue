cdb84ee7866a69f2ec9ebe043a8eecea8b703ce2
Gunnar Oledal <gunnar.oledal@gmail.com>
2025-04-10 13:36:41 +0200

    listening state emitted to registered handler

--- pages/recognitionTest.vue
+++ pages/recognitionTest.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col gap-6 items-center">
-    <div class="mt-52 shrink flex gap-6">
-      <UButton class="w-40" variant="subtle" :color="started ? 'error' : 'primary'"
-        @click="started ? stopListening() : startListening()">{{ started ? 'Stop' : 'Start' }}
+  <div class="flex flex-col gap-6 p-40 items-center h-screen">
+    <div class="shrink flex gap-6">
+      <UButton class="w-40" variant="subtle" :color="listening ? 'error' : 'primary'"
+        @click="listening ? stopListening() : startListening()">{{ listening ? 'Stop' : 'Start' }}
         recognition
         <template #leading>
-          <UIcon class="size-6" name="i-lucide-mic" :class="{ 'animate-pulse': started }" />
+          <UIcon class="size-6" name="i-lucide-mic" :class="{ 'animate-pulse': listening }" />
         </template>
       </UButton>
       <!-- <UButton @click="recognition.stopListenAudio(); started = false">Stop listening</UButton> -->
@@ -24,15 +24,13 @@ const recognition = new WebRecognitionService({
   lang: 'sv-SE',
 })
 
-const started = ref(false);
+const listening = ref(false);
 async function startListening() {
   await recognition.startListenAudio();
-  started.value = true;
 }
 
 function stopListening() {
   recognition.stopListenAudio();
-  started.value = false;
 }
 
 const currentTranscript = ref('');
@@ -44,6 +42,10 @@ recognition.onTextReceived((text) => {
 recognition.onInterimTextReceived((text) => {
   isFinalTranscript.value = false;
   currentTranscript.value = text;
+})
+
+recognition.onListeningStateChanged((state) => {
+  listening.value = state === 'listening' ? true : false;
 })
 
 </script>
--- utils/recognitionService.ts
+++ utils/recognitionService.ts
@@ -1,13 +1,82 @@
 interface STTServiceListenOptions {
   lang?: PossibleLanguages
 }
+
+type ListeningState = 'listening' | 'inactive';
 export interface STTService {
   startListenAudio(options?: STTServiceListenOptions): Promise<void>;
   stopListenAudio(): void;
+  getListeningState(): ListeningState;
   // getTranscript(): string;
+  onListeningStateChanged(handler?: ((state: ListeningState) => void)): void;
   onTextReceived(handler?: ((text: string) => void)): void;
   onInterimTextReceived(handler?: ((text: string) => void)): void;
   // removeTextReceivedListener(listener: (text: string) => void): void;
+}
+
+export class WebRecognitionService implements STTService {
+  private recognition: SpeechRecognition;
+  private defaultListenOptions?: STTServiceListenOptions;
+  constructor(options?: STTServiceListenOptions) {
+    this.defaultListenOptions = options;
+    const SpeechRecognition = window && ((window as any).SpeechRecognition || (window as any).webkitSpeechRecognition)
+    if (!SpeechRecognition) {
+      throw new Error('Speech Recognition is not supported in this browser');
+    }
+    this.recognition = new SpeechRecognition();
+  }
+  async startListenAudio(options?: STTServiceListenOptions) {
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+    this.recognition.lang = options?.lang ?? this.defaultListenOptions?.lang ?? 'en-US';
+    this.recognition.start();
+    this.recognition.onresult = (evt) => {
+      const resultList = evt.results;
+      const latestResult = resultList[resultList.length - 1];
+      const isFinal = latestResult.isFinal;
+      const text = latestResult[0].transcript;
+      if (isFinal) {
+        this.textReceivedHandler?.(text);
+      } else {
+        this.interimTextReceivedHandler?.(text);
+      }
+    }
+    const { promise, resolve, reject } = Promise.withResolvers<void>();
+    this.recognition.onstart = () => {
+      console.log('recognition listen started');
+      this.setListeningState('listening');
+      resolve();
+    }
+    return promise;
+  }
+  stopListenAudio(): void {
+    this.setListeningState('inactive');
+    this.recognition.stop();
+  }
+
+  private listeningState: ListeningState = 'inactive';
+  getListeningState(): ListeningState {
+    return this.listeningState;
+  }
+
+  private listeningStateChangedHandler?: (state: ListeningState) => void;
+  private setListeningState(state: ListeningState): void {
+    this.listeningState = state;
+    this.listeningStateChangedHandler?.(state);
+  }
+  onListeningStateChanged(handler?: (state: ListeningState) => void): void {
+    this.listeningStateChangedHandler = handler;
+  }
+
+  private textReceivedHandler?: (text: string) => void;
+  onTextReceived(handler?: (text: string) => void): void {
+    this.textReceivedHandler = handler;
+  }
+  
+  private interimTextReceivedHandler?: (text: string) => void;
+  onInterimTextReceived(handler?: ((text: string) => void)): void {
+    this.interimTextReceivedHandler = handler;
+  }
 }
 
 export class MockSTTService implements STTService {
@@ -23,7 +92,7 @@ export class MockSTTService implements STTService {
   async startListenAudio() {
     console.log('startListening audio');
     // clear previous interval if is set
-    if(this.currentInterval) clearInterval(this.currentInterval);
+    if (this.currentInterval) clearInterval(this.currentInterval);
 
     this.currentInterval = setInterval(() => {
       console.log('setting interval');
@@ -45,54 +114,6 @@ export class MockSTTService implements STTService {
 
   private interimTextReceivedHandler?: (text: string) => void;
   onInterimTextReceived(handler?: (text: string) => void): void {
-    this.interimTextReceivedHandler = handler;
-  }
-}
-
-export class WebRecognitionService implements STTService {
-  private recognition: SpeechRecognition;
-  private defaultListenOptions?: STTServiceListenOptions;
-  constructor(options?: STTServiceListenOptions) {
-    this.defaultListenOptions = options;
-    const SpeechRecognition = window && ((window as any).SpeechRecognition || (window as any).webkitSpeechRecognition)
-    if (!SpeechRecognition) {
-      throw new Error('Speech Recognition is not supported in this browser');
-    }
-    this.recognition = new SpeechRecognition();
-  }
-  private textReceivedHandler?: (text: string) => void;
-  private interimTextReceivedHandler?: (text: string) => void;
-  async startListenAudio(options?: STTServiceListenOptions) {
-    this.recognition.continuous = true;
-    this.recognition.interimResults = true;
-    this.recognition.lang = options?.lang ?? this.defaultListenOptions?.lang ?? 'en-US';
-    this.recognition.start();
-    this.recognition.onresult = (evt) => {
-      const resultList = evt.results;
-      const latestResult = resultList[resultList.length - 1];
-      const isFinal = latestResult.isFinal;
-      const text = latestResult[0].transcript;
-      if (isFinal) {
-        this.textReceivedHandler?.(text);
-      } else {
-        this.interimTextReceivedHandler?.(text);
-      }
-    }
-    const { promise, resolve, reject } = Promise.withResolvers<void>();
-    this.recognition.onstart = () => {
-      console.log('recognition listen started');
-      resolve();
-    }
-    return promise;
-  }
-  stopListenAudio(): void {
-    this.recognition.stop();
-  }
-  onTextReceived(handler?: (text: string) => void): void {
-    this.textReceivedHandler = handler;
-  }
-  
-  onInterimTextReceived(handler?: ((text: string) => void)): void {
     this.interimTextReceivedHandler = handler;
   }
 }
