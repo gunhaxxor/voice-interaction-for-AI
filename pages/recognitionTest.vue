019918df38e198010b45fb87cc7444fed14f296f
Gunnar Oledal <gunnar.oledal@gmail.com>
2025-04-10 13:24:34 +0200

    Good shit!

--- pages/recognitionTest.vue
+++ pages/recognitionTest.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex flex-col gap-6 items-center">
     <div class="mt-52 shrink flex gap-6">
-      <UButton @click="startListening">Start listening
+      <UButton class="w-40" variant="subtle" :color="started ? 'error' : 'primary'"
+        @click="started ? stopListening() : startListening()">{{ started ? 'Stop' : 'Start' }}
+        recognition
         <template #leading>
-          <UIcon name="i-material-symbols-mic" :class="{ 'animate-pulse': started }" />
+          <UIcon class="size-6" name="i-lucide-mic" :class="{ 'animate-pulse': started }" />
         </template>
       </UButton>
-      <UButton @click="recognition.stopListenAudio(); started = false">Stop listening</UButton>
+      <!-- <UButton @click="recognition.stopListenAudio(); started = false">Stop listening</UButton> -->
     </div>
     <div class="max-w-xl">
       <p :class="{ 'font-bold': isFinalTranscript }">
@@ -26,6 +28,11 @@ const started = ref(false);
 async function startListening() {
   await recognition.startListenAudio();
   started.value = true;
+}
+
+function stopListening() {
+  recognition.stopListenAudio();
+  started.value = false;
 }
 
 const currentTranscript = ref('');
