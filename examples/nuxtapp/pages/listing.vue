f08789270d16ddd9492279bf93f194f52510857a
Gunnar Oledal <gunnar.oledal@gmail.com>
2025-08-12 11:21:37 +0200

    listing and refactoring the kbwhisper name to localwhisper

---
+++ examples/nuxtapp/pages/listing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid place-content-center h-screen gap-5">
+    <ULink v-for="route in otherRoutes" :key="route.name" :to="route.path">{{ route.name }}</ULink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const router = useRouter();
+  const routes = router.getRoutes();
+  const otherRoutes = routes.filter(route => route.name !== router.currentRoute.value.name)
+  console.log(otherRoutes);
+
+
+</script>
+
+<style>
+
+</style>
